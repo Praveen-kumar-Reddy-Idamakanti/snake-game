c7686e03126998403d1c14cf883bcfa375855a11
praveen <ageabove60@gmail.com>
2025-12-30 19:20:51 +0530

    added food feature

--- index.js
+++ index.js
@@ -8,58 +8,80 @@ for(let i=0;i<gridSize;i++){
     board.appendChild(cell);
 }
 
-//game start logic
-const startBtn= document.getElementById("game-start");
-function gamesStart(isStarted){
-    if (isStarted){
-        console.log("GameStarted");
-        startBtn.style.display="block";
-        createSnake();
-    }
-    else{
-        console.log("display startButton")
-    }
+const cols=60
+const rows=30
+
+const centerRow=Math.floor(rows/2)
+const centerCol=Math.floor(cols/2)
+
+const centerIndex=centerRow*cols+centerCol
+
+snakeArr=[centerIndex];
+let direction=1;
+leftWall=[]
+rightWall=[]
+let i=0
+while (i<1800){
+    leftWall.push(i);
+    i=i+60
+    rightWall.push(i);
 }
- 
-function createSnake(){
-    const cols=60
-    const rows=30
-    
-    const centerRow=Math.floor(rows/2)
-    const centerCol=Math.floor(cols/2)
+console.log(leftWall);
 
-    const centerIndex=centerRow*cols+centerCol
-    const centerCell=board.children[centerIndex]
 
-    snake= document.createElement("div");
-    snake.classList.add("snake")
-    centerCell.appendChild(snake)
-    console.log("added in center")
-    coreGame(centerIndex)
-}
 
-function coreGame(centerIndex){
-    snakeArr=[centerIndex];
-    let direction=1;
-    const cols=60
-    //collect key boiard events
+//collect key boiard events
     document.addEventListener("keydown",(e)=>{
         if (e.code=="ArrowUp")direction= -cols;
         if (e.code=="ArrowDown")direction= cols;
         if (e.code=="ArrowRight")direction= 1;
         if (e.code=="ArrowLeft")direction= -1;
+        if (e.code=="Enter")gamesStart(true);
     });
+
+//game start logic
+const startBtn= document.getElementById("game-start");
+function gamesStart(isStarted){
+
+    if (isStarted){
+        console.log("GameStarted");
+        startBtn.style.display="none";
+        resetGame();
+        createFood();
+        coreGame();
+    }
+    else{ //if game is not started
+        startBtn.style.display="block"
+        console.log("display startButton")
+    }
+}
+ 
+function createSnake(){
+    const centerCell=board.children[centerIndex]
+    snake= document.createElement("div");
+    snake.classList.add("snake")
+    centerCell.appendChild(snake)
+    console.log("added in center")
+}
+
+function coreGame(){
+    
     function moveSnake(){
         const newHead=snakeArr[0]+direction;
-        if (newHead<=0 || newHead>=1800 ){
+        if (newHead in leftWall) console.log(newHead);
+        if (newHead<0 || newHead>=1800){
+            console.log(newHead+1);
             gameEnd(intervalID)
+            return;
+        }
+        if ((direction==-1 && leftWall.includes(snakeArr[0]))|| (direction==1 && rightWall.includes(snakeArr[0]+1))){
+            gameEnd(intervalID);
             return;
         }
         snakeArr.unshift(newHead);
         snakeArr.pop();
 
-        document.querySelectorAll(".snake").forEach(s => s.remove());//remove the all current class with .snake
-
+        removeSnake();
         snakeArr.forEach(element => {
             const s= document.createElement("div");
             s.classList.add("snake")
@@ -68,7 +90,37 @@ function coreGame(centerIndex){
 
     }
     const intervalID=setInterval(moveSnake,90);
+    
+}
+function createFood(){
+    document.querySelectorAll(".food").forEach(f => f.remove());
+    min=0
+    max=1799
+    let rand_cell;
+    do{
+        rand_cell=Math.floor(Math.random()*(max-min)+min);
 
+    }while (snakeArr.includes(rand_cell));
+    const cell=board.children[rand_cell]
+    food=document.createElement("div");
+    food.classList.add("food");
+    cell.appendChild(food);
+
+    food_index=rand_cell;
+    
+    console.log("added food");
+}
+
+function removeSnake(){
+    document.querySelectorAll(".snake").forEach(s => s.remove());//remove the all current class with .snake
+}
+
+function resetGame(){
+    direction=1;
+    snakeArr=[centerIndex]
+    removeSnake();
+    createSnake();
+    gamesStart(false)
 }
 
 //game end logic
@@ -77,6 +129,6 @@ function gameEnd(intervalID){
     alert("Game Failed")
     clearInterval(intervalID);
     //restart the game
-    gamesStart(false)
+    gamesStart(true);
 }
 
